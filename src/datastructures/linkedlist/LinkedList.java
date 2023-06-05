55896f780ebb8f262e82f5646ac369d83d528e50
Shashank Sharma <shashanksharma.er104@gmail.com>
2023-06-05 13:59:21 +0530

    Added removeLast and prepend methods

--- src/datastructures/linkedlist/LinkedList.java
+++ src/datastructures/linkedlist/LinkedList.java
@@ -55,4 +55,34 @@ public class LinkedList {
         length++;
     }
 
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while(temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if(length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
+    }
+
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if(length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
 }
--- src/datastructures/linkedlist/Main.java
+++ src/datastructures/linkedlist/Main.java
@@ -6,13 +6,20 @@ public class Main {
         LinkedList myLinkedList = new LinkedList(4);
 
         myLinkedList.append(6);
+        //myLinkedList.append(8);
 
         myLinkedList.printList();
 
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
+        /*System.out.println("Removing the values");
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast().value);
+        System.out.println(myLinkedList.removeLast());*/
+
+        System.out.println("Prepend the node");
+        myLinkedList.prepend(2);
+        myLinkedList.prepend(1);
+        myLinkedList.printList();
 
     }
-
 }
